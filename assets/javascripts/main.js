c1519810e4977cad9f70d60b8df6ff14ba369c8b
Shrishti Mishra <mishrashrishti0801@gmail.com>
2025-08-30 06:26:45 +0530

    Add files via upload

---
+++ assets/javascripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$("#main-nav a").click(function(){
+  $("section").removeClass("show");
+   var target = $(this).attr("href");
+   $(target).addClass("show");
+});
+
+$(".type-effect").typed({
+  strings:["Full Stack Developer", "Data Scientist"," Designer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true
+})
+
+});
+
